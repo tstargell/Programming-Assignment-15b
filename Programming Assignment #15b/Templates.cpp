72f82aa4d7cd7e8214fac05fac83d0ecbb585cef
tstargell <taylor.stargell@sinclair.edu>
2023-12-01 23:34:22 -0500

    Add project files.

---
+++ Programming Assignment #15b/Templates.cpp
@@ -0,0 +1,25 @@
+// Taylor Stargell
+// CIS 1202 R02
+// December 1, 2023
+
+#include <iostream>
+using namespace std;
+
+// Function prototype
+half()
+
+int main()
+{
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
+}
+
+half
+{
+
+}
